fa07e02ef27a55ac8ff13713a9a10698586abf69
Lucas <zamaorigin@gmail.com>
2022-04-17 02:30:34 -0300

    BaSP-W05-2022: Finished the first block of exercises, Variables and Operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+// Variables y Operadores
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var num1 = 15;
+var num2 = 20;
+var suma = num1 + num2;
+console.log(suma);
+
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var string1 = "First string";
+var string2 = "Second string";
+var string3 = string1 + " " + string2
+console.log(string3);
+
+// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+var firstString = "Hola";
+var secondString = "Buenas noches";
+var sumaStrings = firstString.length + secondString.length;
+console.log(sumaStrings);
---
+++ Semana-05/exercises/02_strings.js
@@ -0,0 +1,8 @@
+// Strings
+// Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
+
+var stringMinus = "Este string será convertido a mayúsculas";
+console.log(stringMinus.toUpperCase());
+
+// Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+
