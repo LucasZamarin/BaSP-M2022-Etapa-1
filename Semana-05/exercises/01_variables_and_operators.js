cb99c65da2b77736012a32c552ed36f27f2e4df5
Lucas <zamaorigin@gmail.com>
2022-04-18 00:57:22 -0300

    BaSP-W05-2022: ex01-variables and operators

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -1,21 +1,29 @@
+console.log('EXERCISE 1: VARIABLES AND OPERATORS');
+
 // Variables y Operadores
-// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('Exercise 1.a:');
 
 var num1 = 15;
 var num2 = 20;
 var suma = num1 + num2;
 console.log(suma);
 
-// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
-var string1 = "First string";
-var string2 = "Second string";
+console.log('Exercise 1.b:');
+
+var string1 = 'First string';
+var string2 = 'Second string';
 var string3 = string1 + " " + string2
 console.log(string3);
 
-// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
 
-var firstString = "Hola";
-var secondString = "Buenas noches";
+console.log('Exercise 1.c:');
+
+var firstString = 'Hello';
+var secondString = 'Good Night';
 var sumaStrings = firstString.length + secondString.length;
 console.log(sumaStrings);
